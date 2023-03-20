0b5acce32cfb66fed8e4f763f0f94ef70944e063
m_ronald <michael.ronald@allycode.com.br>
2023-03-20 19:58:05 -0300

    add specification repository | reorder files using use cases

---
+++ src/modules/cars/repositories/SpecificationRepository.ts
@@ -0,0 +1,36 @@
+import { Specification } from "../model/Specification";
+import { ICreateSpecificationDTO, ISpecificationRepository } from "./ISpecificationRepository";
+
+class SpecificationRepository implements ISpecificationRepository {
+    private specifications: Specification[];
+
+    constructor() {
+        this.specifications = [];
+    }
+
+    create({ name, description }: ICreateSpecificationDTO): void {
+        const specification = new Specification();
+
+        Object.assign(specification, {
+            name,
+            description,
+            created_at: new Date(),
+        });
+
+        this.specifications.push(specification);
+    }
+
+    list(): Specification[] {
+        throw new Error("Method not implemented.");
+    }
+
+    findByName(name: string): Specification {
+        const specification = this.specifications.find(
+            (specification) => specification.name === name
+        );
+
+        return specification;
+    }
+}
+
+export { SpecificationRepository };
