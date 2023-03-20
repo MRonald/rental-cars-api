e46ef3216bf27908cb77263d3bf033d84dcc7f95
m_ronald <michael.ronald@allycode.com.br>
2023-03-20 18:33:44 -0300

    reorder files using modules

---
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -0,0 +1,12 @@
+interface IRequest {
+    name: string;
+    description: string;
+}
+
+class CreateSpecificationService {
+    execute(): void {
+        console.log('TODO');
+    }
+}
+
+export { CreateSpecificationService };
