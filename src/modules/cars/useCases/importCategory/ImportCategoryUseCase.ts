357bf5facd18e6084418a1a8945a634bf9300f81
m_ronald <michael.ronald@allycode.com.br>
2023-03-21 20:51:04 -0300

    using stream and csv parse for read files

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,10 +1,20 @@
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+import fs from 'fs';
+import { parse as csvParse } from "csv-parse";
 
 class ImportCategoryUseCase {
     constructor(private categoryRepository: ICategoriesRepository) {}
 
-    execute(file: any) {
-        console.log(file);
+    execute(file: Express.Multer.File): void {
+        const stream = fs.createReadStream(file.path);
+
+        const parseFile = csvParse();
+
+        stream.pipe(parseFile);
+
+        parseFile.on('data', async (line) => {
+            console.log(line);
+        })
     }
 }
 
