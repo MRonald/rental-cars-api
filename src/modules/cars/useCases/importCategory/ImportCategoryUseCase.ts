a47836352d70833cf23b2f91ef09935f5ea1f12a
m_ronald <michael.ronald@allycode.com.br>
2023-03-21 21:11:14 -0300

    finish data import from file

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -2,19 +2,53 @@ import { ICategoriesRepository } from "../../repositories/ICategoriesRepository"
 import fs from 'fs';
 import { parse as csvParse } from "csv-parse";
 
+interface IImportCategory {
+    name: string;
+    description: string;
+}
+
 class ImportCategoryUseCase {
     constructor(private categoryRepository: ICategoriesRepository) {}
 
-    execute(file: Express.Multer.File): void {
-        const stream = fs.createReadStream(file.path);
+    loadCategory(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
+            const categories: IImportCategory[] = [];
 
-        const parseFile = csvParse();
+            const parseFile = csvParse();
+        
+            stream.pipe(parseFile);
+        
+            parseFile.on('data', async (line) => {
+                const [name, description] = line;
 
-        stream.pipe(parseFile);
+                categories.push({
+                    name, 
+                    description,
+                });
+            }).on('end', () => {
+                resolve(categories);
+            }).on('error', (err) => {
+                reject(err);
+            });
+        });
+    }
 
-        parseFile.on('data', async (line) => {
-            console.log(line);
-        })
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.loadCategory(file);
+        
+        categories.map(async (category) => {
+            const { name, description } = category;
+
+            const existsCategory = this.categoryRepository.findByName(name);
+
+            if (!existsCategory) {
+                this.categoryRepository.create({
+                    name,
+                    description
+                });
+            }
+        });
     }
 }
 
