3ac6c760848d3fc5a1fb8252149b04f43d416821
m_ronald <michael.ronald@allycode.com.br>
2023-04-05 08:20:40 -0300

    create user model and migration

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,36 @@
+import { Column, CreateDateColumn, PrimaryColumn } from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+
+class User {
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    name: string;
+    
+    @Column()
+    username: string;
+        
+    @Column()
+    email: string;
+            
+    @Column()
+    password: string;
+                
+    @Column()
+    driver_license: string;
+                    
+    @Column()
+    is_admin: string;
+                        
+    @CreateDateColumn()
+    created_at: string;
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuidV4();
+        }
+    }
+}
+
+export { User };
