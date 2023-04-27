85b2967757bb2cc7b935e8c56bb4223103982a96
m_ronald <michael.ronald@allycode.com.br>
2023-04-27 07:07:27 -0300

    create user repository, controller and use case

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,29 @@
+import { inject, injectable } from "tsyringe";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+
+@injectable()
+class CreateUserUseCase {
+    constructor(
+        @inject('UsersRepository')
+        private usersRepository: IUsersRepository
+    ) {}
+
+    async execute({
+        name, 
+        username, 
+        email, 
+        driver_license, 
+        password 
+    }: ICreateUserDTO): Promise<void> {
+        this.usersRepository.create({
+            name, 
+            username, 
+            email, 
+            driver_license, 
+            password 
+        });
+    }
+}
+
+export { CreateUserUseCase };
