61ec38bc231eee41407df93cce6e698a6c30ac25
m_ronald <michael.ronald@allycode.com.br>
2023-05-04 19:35:32 -0300

    creating middleware for user authentication and using on specification routes

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { ListSpecificationController } from "../modules/cars/useCases/listSpecification/ListSpecificationController";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationController = new ListSpecificationController();
+
+specificationsRoutes.use(ensureAuthenticated);
 
 specificationsRoutes.post('/', createSpecificationController.handle);
 
